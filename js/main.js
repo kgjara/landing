3e53827327c7579c30c2dbe0a5638add61ebae51
Kenny Jara <84399723+kgjara@users.noreply.github.com>
2024-06-12 02:55:48 -0500

    Cambios html, JS

--- js/main.js
+++ js/main.js
@@ -1,18 +1,13 @@
 let loaded = (eventLoaded) => {
+  const myform = document.getElementById('comic-form');
 
-  /*  window.alert("landing page loaded");
-    console.log(eventLoaded);*/
-
-    /*loaded
-    
-    */
-  let myform = document.getElementById('comic-form');
   myform.addEventListener('submit', (eventSubmit) => {
     eventSubmit.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const favorito = document.getElementById('favorito').value;
 
-    let nombreValue = nombre.value;
-
-    if (nombreValue.length == 0) {
+    if (nombre.length == 0) {
 
       nombre.focus()
 
@@ -20,9 +15,8 @@ let loaded = (eventLoaded) => {
 
       return;
     }
-    let emailValue = email.value;
 
-    if (emailValue.length == 0) {
+    if (email.length == 0) {
 
       email.focus()
 
@@ -30,19 +24,51 @@ let loaded = (eventLoaded) => {
 
       return;
     }
-    let recomendacionValue = recomendacion.value;
 
-    if (recomendacionValue.length == 0) {
+    const datos = {
+      nombre: nombre,
+      email: email,
+      favorito: favorito
+    };
 
-      recomendacion.focus()
+    fetch('https://deep-clock-284401-default-rtdb.firebaseio.com/coleccion.json', {
+      method: 'POST',
+      body: JSON.stringify(datos),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(respuesta => respuesta.json())
+      .then(datos => {
+        console.log(datos);
+      })
+      .catch(error => console.error(error));
 
-      alert('Ingrese un texto válido')
+  })
+}
+window.addEventListener("DOMContentLoaded", loaded);
 
-      return;
-    }
+async function obtenerDatos() {
+  const url = "https://deep-clock-284401-default-rtdb.firebaseio.com/coleccion.json";
+  const respuesta = await fetch(url);
+  if (!respuesta.ok) {
+    console.error("Error:", respuesta.status);
+    return;
+  }
+  const datos = await respuesta.json();
+  let favoritoTotales = new Map();
+  for(var key in datos){
+    let elementos = datos[key];
+    let favorito = elementos["favorito"];
+    let conteo = favoritoTotales.has(favorito)?favoritoTotales.get(favorito)+1:1;
+    favoritoTotales.set(favorito,conteo);
+  }
+  console.log(datos); // Procesar o mostrar los datos obtenidos
+}
+obtenerDatos();
+
 // lo siguiente encapsularla en una funcion submit
 //tiene que ser con el evento submit no con el click
-//La recomendacion debe ser un select
 /*submitValues=()=>{
   const datos{
 
@@ -51,7 +77,7 @@ let loaded = (eventLoaded) => {
 
 let loadVotes() =async()=>{};
 
-*/
+
     const formulario = document.getElementById('formulario');
     formulario.addEventListener('submit', (event) => {
       event.preventDefault();
@@ -81,14 +107,6 @@ let loadVotes() =async()=>{};
 
   })
 
-  let template = `
-		<tr>
-			<td>${recomendacion}</td>
-			<td>${conteo}</td>
-		</tr>
-	`
-}
-window.addEventListener("DOMContentLoaded", loaded);
 
 async function obtenerDatos() {
   const url = "https://www.ejemplo.com/"; // Reemplaza con la URL real de la API o recurso
@@ -101,6 +119,6 @@ async function obtenerDatos() {
   console.log(datos); // Procesar o mostrar los datos obtenidos
   }
 
-  obtenerDatos();
+  obtenerDatos();*/
 
 
